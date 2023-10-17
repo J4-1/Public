c9cb30df266b66dcfb529e7ba953c39fa05cda0a
J4-1 <105065524+J4-1@users.noreply.github.com>
2023-10-17 21:21:36 +0000

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -6,3 +6,7 @@
       document.getElementById('button2').addEventListener('click', function() {
         alert('Button 2 clicked!');
       });
+
+      document.getElementById('button3').addEventListener('click', function() {
+        alert('Button 3 clicked!');
+      });
