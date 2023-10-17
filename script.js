7331167ff88b5281324fb2e5f174fab62e652d2d
J4-1 <105065524+J4-1@users.noreply.github.com>
2023-10-17 15:46:51 -0500

    Updated Structure

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// Coming Soon
+    // JavaScript for button click events
+    document.getElementById('button1').addEventListener('click', function() {
+        alert('Button 1 clicked!');
+      });
+  
+      document.getElementById('button2').addEventListener('click', function() {
+        alert('Button 2 clicked!');
+      });
